1186737b1604cce755960eacf993cf2eea86d111
Yurii <yura.gvozd.2002@gmail.com>
2026-09-17 23:45:12 +0300

    'Solution'

---
+++ cinema/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if not (request.user and request.user.is_authenticated):
+            return False
+        if request.user.is_staff:
+            return True
+        return request.method in SAFE_METHODS
+
+
+class IsAdminOrIfAuthenticatedReadOnlyOrCreate(BasePermission):
+    def has_permission(self, request, view):
+        if not (request.user and request.user.is_authenticated):
+            return False
+        if request.user.is_staff:
+            return True
+        return request.method in SAFE_METHODS or request.method == "POST"
